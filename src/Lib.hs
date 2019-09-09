51aa06d6e5ac076f742c31f99455df09e61906a4
Vladislav Prekel <misterptits@yandex.ru>
2019-09-09 21:38:18 +0700

    Factorial

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,11 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    line <- getLine
+    putStrLn ("you said: " ++ line)
+    let x = read line :: Integer
+    print (factorial x)
+
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
