ebff2c4dc7dee07249a83935e3ddfad9ca867f26
Vladislav Prekel <misterptits@yandex.ru>
2019-09-10 21:44:56 +0700

    Quadratuc & linear

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,32 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+--main = someFunc
+main = print "123"
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+a' :: (Num a) => [a] -> a
+a' [] = 0
+a' (x:xs) = x + sum' xs
+
+linear a b 
+    | a == 0 = []
+    | otherwise = [-b / a]
+
+quadratic a b c
+    | a == 0 = []
+    | det == 0 = [-b / (2 * a)]
+    | det > 0  = [(-b - sqrt det) / (2 * a), (-b + sqrt det) / (2 * a)]
+    | otherwise = []
+    where det = b ^ 2 - 4 * a * c
+
+checker a b c x = a * x * x + b * x + c
+
+checker' x f 
+    | f x == 0 = True
+    | otherwise = False
+
+--quadratic' a b c = [(-b - det) / (2 * a), (-b + det) / (2 * a), let det = b ^ 2 - 4 * a * c]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,21 @@
 module Lib
-    ( someFunc
+    (
+--    ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = do
-    line <- getLine
-    putStrLn ("you said: " ++ line)
-    let x = read line :: Integer
-    print (factorial x)
+--someFunc :: IO ()
+--someFunc = do
+--    line <- getLine
+ --   putStrLn ("you said: " ++ line)
+ --   let x = read line :: Integer
+--    print (factorial x)
+--    print (linear x 6)
 
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
+
+--linear :: (Num a, Num a) => a -> a -> [a]
+--linear a b
+--    | a * b == 0 = [-b / a]
+--    | otherwise = []
+
