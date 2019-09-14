0bd9c82805a66340feef358506fcd76fd583076f
Vladislav Prekel <misterptits@yandex.ru>
2019-09-14 13:24:42 +0700

    "MyExpression" equation solver

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,7 @@ main = do
     let c = read line :: Double
     print (quadratic a b c)
 
+linear :: (Num i, Eq i, Fractional i) => i -> i -> [i]
 linear a b
     | a == 0 && b == 0 = error "Infinite number of roots"
     | a == 0 && b /= 0 = []
@@ -29,7 +30,7 @@ quadratic a b c
     | otherwise = [(-b - sqrt d) / (2 * a), (-b + sqrt d) / (2 * a)]
     where d = discriminant a b c 
 
-checker a b c x = a * x * x + b * x + c
+checker a b c x = a * x * x + b * x + c == 0
 
 checker' x f 
     | f x == 0 = True
@@ -42,4 +43,62 @@ maximum' (x:xs)
     | x > maxTail = x  
     | otherwise = maxTail  
     where maxTail = maximum' xs
-    
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+polynomialCalc :: (Num i) => [i] -> i -> i
+polynomialCalc [] x = 0
+polynomialCalc [g] x = g
+polynomialCalc (g:gs) x = g * x ^ length gs + polynomialCalc gs x
+
+polynomialDerivative :: (Num i) => [i] -> [i]
+polynomialDerivative [] = []
+polynomialDerivative [x] = []
+polynomialDerivative (x:xs) = (x * fromIntegral(length xs)) : polynomialDerivative xs
+
+polynomialSolve :: (Eq i, Fractional i, Ord i) => [i] -> i -> [i]
+polynomialSolve [] _ = error "Empty polynomial"
+--polynomialSolve [0] = error "Any root"
+polynomialSolve [x] _ = error "No root"
+polynomialSolve (a:b:[]) _ = linear a b
+polynomialSolve p eps = [binarySearch polynomialCalc' (fst x) (snd x) eps | x <- mIntervals]
+    where derRoots = polynomialSolve (polynomialDerivative p) eps
+          mIntervals = monotonyIntervals (derRoots)
+          polynomialCalc' x = polynomialCalc p x
+
+positiveInf :: Fractional a => a
+positiveInf = 1 / 0
+negativeInf :: Fractional a => a
+negativeInf = -1 / 0
+
+approxPositiveInf :: Fractional a => a
+approxPositiveInf = 2^32
+approxNegativeInf :: Fractional a => a
+approxNegativeInf = -2^32
+
+binarySearch
+  :: (Ord t, Ord a, Fractional t, Num a) =>
+     (t -> a) -> t -> t -> t -> t
+binarySearch f left right eps
+    | left == negativeInf      = binarySearch f approxNegativeInf right eps
+    | right == positiveInf     = binarySearch f left approxPositiveInf eps
+    | abs (left - right) < eps = median
+    | mValue > 0 = binarySearch f left median eps
+    | otherwise                = binarySearch f median right eps
+    where median = (left + right) / 2
+          mValue = isPositive * f median
+          isPositive = if (f right - f left) > 0 then 1 else (-1)
+
+
+monotonyIntervals :: (Fractional a) => [a] -> [(a, a)]
+monotonyIntervals [] = [(negativeInf, positiveInf)]
+monotonyIntervals [x] = [(negativeInf, x), (x, positiveInf)]
+monotonyIntervals (x:xs) = [(negativeInf, x)] ++ [(x, snd (head mixs))] ++ tail mixs
+    where mixs = monotonyIntervals xs
+
+monotonyIntervals' :: (Fractional a) => [a] -> [(a, a)]
+monotonyIntervals' x = take (length mix - 2) (tail (mix))
+    where mix = monotonyIntervals x
