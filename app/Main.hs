72d98d618c12cfddfbb8b0fe72e5af0fe5865531
Vladislav Prekel <misterptits@yandex.ru>
2019-09-15 22:25:31 +0700

    Cyrillic to latin

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,10 @@ import Lib
 
 main :: IO ()
 main = do
-    putStrLn "Введите точность eps: "
+    putStrLn "Enter eps (Example: 1e-8): "
     line <- getLine
     let eps = read line :: Double
-
-    putStrLn ("Введите через запятую коэффициенты уравнения от коэффициента при большей степени до меньшей: ")
+    putStrLn "Enter coefficients of equation from coefficient at greater degree (Example: 1,-2,-1,2 for x^4-2x^3-x+2=0): "
     line <- getLine
     let poly = read ("[" ++ line ++ "]") :: [Double]
-
-    print (polynomialSolve poly eps)
+    print ("Roots list: " ++ show (polynomialSolve poly eps))
