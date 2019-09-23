f82388304768a5397c6d5e7a0ea7d7409513a529
Vladislav Prekel <misterptits@yandex.ru>
2019-09-23 17:32:57 +0700

    All variants

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,34 @@
 module Lib
     (
+        secondElement,
+        thirdElement,
+        fourthElement,
+        longerThan,
+        distr,
+        swapFirstLast,
     ) where
 
+-- | Вариант 1
+secondElement a = head (tail a)
+thirdElement a = head (tail (tail a))
+fourthElement a = head (tail (tail (tail a)))
+
+-- | Вариант 2 (longer_than)
+longerThan a b = sum [1 | x <- a] > sum [1 | x <- b]
+
+-- | Вариант 3
+distr [ar1, ar2, ar3, ar4] = [[ar1, ar2], [ar3, ar4]]
+distr [ar1, ar2] = [[ar1], [ar2]]
+distr [ar1] = [[ar1]]
+
+-- | Вариант 4 (swap-first-last)
+swapFirstLast list = swapFirstLast' [] list
+    where
+        swapFirstLast' a [] = []
+        swapFirstLast' a [x] = [x]
+        swapFirstLast' a [x, y] = [y] ++ a ++ [x]
+        swapFirstLast' a b = swapFirstLast' (a ++ [second]) (first : tailTail)
+            where
+                first = head b
+                second = head (tail b)
+                tailTail = tail (tail b)
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,10 +4,6 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
-eps = 1e-8
-f1 x = 1 * x * x * x - 2 * x * x - 1 * x + 2
-p1 = [1, -2, -1, 2]
-a1 = polynomialSolve p1 eps
 
 main :: IO ()
 main = hspec $ do
@@ -21,5 +17,3 @@ main = hspec $ do
     it "throws an exc   eption if used with an empty list" $ do
       evaluate (head []) `shouldThrow` anyException
       
-    it ("1231e12" ++ show p1 ++ " " ++ show a1) $ do
-       f1 (head a1) < eps `shouldBe` True
