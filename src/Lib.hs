f1ed25315852be12deab76ece055e3bd0e054fcd
Vladislav Prekel <misterptits@yandex.ru>
2019-10-14 22:43:46 +0700

    fixes

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,27 +2,15 @@ module Lib
     (
     multifactorialRec,
     multifactorialList,
-    factorialRec,
-    factorialList,
     ) where
 
----- | Вариант 4
+-- | Вариант 4
 
 multifactorialRec n k
     | 0 < n && n <= k = n
-    | n > k = n * (multifactorialRec (n - k) k)
-    | otherwise = error "Нельзя неположительные"
+    | n > k           = n * multifactorialRec (n - k) k
+    | otherwise       = error "Нельзя неположительные"
 
 multifactorialList n k
     | n <= 0 || k <= 0 = error "Нельзя неположительные"
-    | otherwise = product [n, n - k..1]
-
-factorialRec n
-    | n == 0 = 1
-    | n > 0 = n * factorialRec(n - 1)
-    | otherwise = error "Нельзя отрицательные"
-    
-factorialList n
-    | n < 0 = error "Нельзя отрицательные"
-    | otherwise = product [1..n]
-    
+    | otherwise        = product [n, n - k .. 1]
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,11 +3,23 @@ import Lib
 import Test.Hspec
 import Test.QuickCheck
 
+-- | Функции факториала для проверки кратного факториала по свойству разложения факториала через кратный факториал
+-- | Разложение факториала через кратный факториал: https://wikimedia.org/api/rest_v1/media/math/render/svg/3c3ece73871dfdfecd7eb8f01c190f66d8812f64
+
+factorialRec n
+    | n == 0    = 1
+    | n > 0     = n * factorialRec(n - 1)
+    | otherwise = error "Нельзя отрицательные"
+    
+factorialList n
+    | n < 0     = error "Нельзя отрицательные"
+    | otherwise = product [1..n]
+
 main :: IO ()
 main = hspec $ do
-    it "Check 5! = 5!!! * (5 - 1)!!! * (5 - 2)!!! (recursive)" $ do
+    it "Check 5! = 5!!! * 4!!! * 3!!! (recursive)" $ do
         factorialRec 5 `shouldBe` multifactorialRec 5 3 * multifactorialRec 4 3 * multifactorialRec 3 3
-    it "Check 5! = 5!!! * (5 - 1)!!! * (5 - 2)!!! (list comprehension)" $ do
+    it "Check 5! = 5!!! * 4!!! * 3!!! (list comprehension)" $ do
         factorialList 5 `shouldBe` multifactorialList 5 3 * multifactorialList 4 3 * multifactorialList 3 3
     it "Check 5! = 1 * 2 * 3 * 4 * 5 (recursive)" $ do
         factorialRec 5 `shouldBe` 1 * 2 * 3 * 4 * 5
@@ -17,9 +29,9 @@ main = hspec $ do
         factorialRec 5 `shouldBe` 120
     it "Check 5! = 120 (list comprehension)" $ do
         factorialList 5 `shouldBe` 120
-    it "Check 10! = 10!! * (10 - 1)!! (recursive)" $ do
+    it "Check 10! = 10!! * 9!! (recursive)" $ do
         factorialRec 10 `shouldBe` multifactorialRec 10 2 * multifactorialRec 9 2
-    it "Check 10! = 10!! * (10 - 1)!! (list comprehension)" $ do
+    it "Check 10! = 10!! * 9!! (list comprehension)" $ do
         factorialList 10 `shouldBe` multifactorialList 10 2 * multifactorialList 9 2
     it "Check 10! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10 (recursive)" $ do
         factorialRec 10 `shouldBe` 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10
