624174301bf70ed7b7d3681ef3e62976efefc3e9
Vladislav Prekel <misterptits@yandex.ru>
2019-10-27 19:14:52 +0700

    var 6

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,20 @@
 module Lib
     (
-    multifactorialRec,
-    multifactorialList,
+    subfactorialRec,
+    subfactorialList,
     ) where
 
--- | Вариант 4
+subfactorialRec n = subfactorialRec' n n
+    where subfactorialRec' n k 
+            | k < 0 = 0
+            | otherwise = minusOne k * factorialAdivFactorialB n k + subfactorialRec' n (k - 1)
+          factorialAdivFactorialB n k
+            | n == k = 1
+            | otherwise = n * factorialAdivFactorialB (n - 1) k
+          minusOne k = if even k then 1 else -1
 
-multifactorialRec n k
-    | 0 < n && n <= k = n
-    | n > k           = n * multifactorialRec (n - k) k
-    | otherwise       = error "Нельзя неположительные"
-
-multifactorialList n k
-    | n <= 0 || k <= 0 = error "Нельзя неположительные"
-    | otherwise        = product [n, n - k .. 1]
+subfactorialList n = sum [minusOne k * factorialAdivFactorialB n k | k <- [0 .. n]]
+    where factorialAdivFactorialB n k
+            | n == k = 1
+            | otherwise = product [(k + 1) .. n]
+          minusOne k = if even k then 1 else -1
