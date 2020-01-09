e3b24f69432a5a5f3fab943c8492936b86d6c598
Vladislav Prekel <misterptits@yandex.ru>
2020-01-09 21:38:13 +0700

    omg

--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,31 @@
 module Main where
 
-import Prelude hiding (catch)
-import Data.List (sortBy)
-import System.Environment (getArgs)
-import System.Directory (doesFileExist)
-import System.IO (hFlush, stdout)
-import System.Exit
-import Control.Exception
-import Control.DeepSeq
-
-import Control.Parallel
-import Control.Monad
-import Text.Printf
-
 import Data.Matrix
 
-matrixSum :: (Num a) => Matrix a -> Matrix a -> Matrix a
-matrixSum a b = matrix (nrows a) (ncols a) $ \(i, j) -> getElem i j a + getElem i j b
+import Matrix
 
-cutoff = 35
+import System.Environment
+import Control.Parallel (par)
 
-fib' :: Int -> Integer
-fib' 0 = 0
-fib' 1 = 1
-fib' n = fib' (n-1) + fib' (n-2)
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> putStrLn "ASD"
+        (firstMatrixString : secondMatrixString : _) -> do
+                                                       let firstMatrix = fromLists (read firstMatrixString :: [[Int]])
+                                                           secondMatrix = fromLists (read secondMatrixString :: [[Int]])
+                                                           sum = matrixSum firstMatrix secondMatrix
+                                                           det = detLaplace sum
+                                                           sumString = show (toLists sum)
+                                                       putStrLn sumString
+                                                       print det
 
-fib :: Int -> Integer
-fib n | n < cutoff = fib' n
-      | otherwise  = r `par` (l `pseq` l + r)
- where
-    l = fib (n-1)
-    r = fib (n-2)
-
-main = forM_ [0..45] $ \i ->
-            printf "n=%d => %d\n" i (fib i)
-            
+pfib :: Int -> Int
+pfib n 
+    | n <= 1    = 1
+    | otherwise = n1 `par` n2 `seq` n1+n2
+    where 
+        n1 = pfib (n-1) 
+        n2 = pfib (n-2)
+        
--- src/Matrix.hs
+++ src/Matrix.hs
@@ -1,7 +1,25 @@
 module Matrix where
 
+import Control.Parallel
+import Control.Parallel.Strategies
+import Data.List
 import Data.Matrix
 
 matrixSum :: (Num a) => Matrix a -> Matrix a -> Matrix a
 matrixSum a b = matrix (nrows a) (ncols a) $ \(i, j) -> getElem i j a + getElem i j b
 
+detLaplace1 :: Num a => Matrix a -> a
+detLaplace1 m = sum1 [(-1) ^ (i - 1) * m ! (i, 1) * detLaplace (minorMatrix i 1 m) | i <- [1 .. nrows m]]
+  where
+    sum1 = foldl1' (+)
+
+qwe1 f xs = map f xs `using` parList rdeepseq
+
+f1 a = a * 2
+xs1 = [1,2,3]
+
+det m = sum [(-1) ^ (i - 1) * m ! (i, 1) * j | i <- [1 .. nrows m], j <- res1] / 3
+  where
+    res1 = qwe1 f2 xs2
+    f2 = detLaplace
+    xs2 = [minorMatrix i 1 m | i <- [1 .. nrows m]]
